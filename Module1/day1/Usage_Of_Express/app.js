8bcd2e3255a0a76868a537de35efbe8a3ed4566b
Anwar-3108 <syed.sahab9628@gmail.com>
2024-05-29 21:12:27 +0530

    added express

---
+++ Module1/day1/Usage_Of_Express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app= express();
+const PORT= 4000
+app.get('/',(_,res)=>{
+res.send('this is homepage');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
