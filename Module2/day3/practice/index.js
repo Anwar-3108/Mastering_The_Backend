41da66211d302a034587cc7f5450a1420f8e1b59
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-08 23:41:20 +0530

    task manager

--- Module2/day3/practice/index.js
+++ Module2/day3/practice/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const path = require("path");
 const port = 3000;
@@ -9,7 +10,24 @@ app.use(express.static(path.join(__dirname, "public")));
 // console.log(path.join(__dirname, "public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir("./files", (err, files) => {
+    console.log(files);
+    res.render("index", { files: files });
+  });
+});
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8",(err, filedata) => {
+    res.render('show' ,{filedata:filedata})
+  });
 });
 
 app.listen(port, () => {
