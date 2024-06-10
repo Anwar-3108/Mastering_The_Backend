de6951d91532afa91e60ef3c9e9554cf6e596d23
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-11 01:46:43 +0530

    added edit file fuctionality

--- Module2/day3/practice/index.js
+++ Module2/day3/practice/index.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
     res.render("index", { files: files });
   });
 });
+
 app.post("/create", (req, res) => {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
@@ -24,9 +25,22 @@ app.post("/create", (req, res) => {
     }
   );
 });
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+});
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+});
+
 app.get("/files/:filename", (req, res) => {
-  fs.readFile(`./files/${req.params.filename}`, "utf-8",(err, filedata) => {
-    res.render('show' ,{filedata:filedata})
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filedata: filedata });
   });
 });
 
