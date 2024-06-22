14a5e5f9d53bb3f57bbf24b54f083736d2dad35b
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-22 23:55:49 +0530

    added routes and connected to db  , sepration of concerns

--- Module2/Scatch/app.js
+++ Module2/Scatch/app.js
@@ -1,21 +1,44 @@
+//======= Imports ========//
 const express = require("express");
 const app = express();
-
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const port = 3000;
 
+//========//
+const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+//======= Imports ========//
+
+//======= Port ========//
+const port = 3000;
+//======= Port ========//
+
+//======= Middelwares ========//
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+//======= Middelwares ========//
 
+//======= Set Engine ========//
 app.set("view engine", "ejs");
+//======= Set Engine ========//
 
-app.get("/", (req, res) => {
-  res.send("hey");
-});
+//======= Routes ========//
+// app.get("/", (req, res) => {
+//   res.send("hey");
+// });
 
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+//======= Routes ========//
+
+//======= Listening Server ========//
 app.listen(port, () => {
   console.log(`app is listening on http://localhost:${port}`);
 });
+//======= Listening Server ========//
