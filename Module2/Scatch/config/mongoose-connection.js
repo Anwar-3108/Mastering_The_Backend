6accc8f188a3509c70787b646fb3c26557073bdd
Anwar-3108 <syed.sahab9628@gmail.com>
2024-06-25 01:27:04 +0530

    enviornment variable

--- Module2/Scatch/config/mongoose-connection.js
+++ Module2/Scatch/config/mongoose-connection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const debug = require("debug")("development:mongoose");
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch")
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
   .then(() => {
-    console.log(" database connected");
+    debug(" database connected");
   })
   .catch((err) => {
-    console.log(err);
+    debug(err);
   });
 module.exports = mongoose.connection;
